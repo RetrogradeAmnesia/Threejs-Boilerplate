4bd73f88cc91d358bc63b7fd12119bf695758f82
Retrograde Amnesia <31753563+RetrogradeAmnesia@users.noreply.github.com>
2021-03-12 18:15:32 +1100

    Port Update

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-app.listen(() =>
-  console.log('Visit http://127.0.0.1')
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,() =>
+  console.log(`Visit http://127.0.0.1:${PORT}`)
 );
