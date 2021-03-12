0f4019edcc8df84d2ecb69e4c05328a1da806338
Retrograde Amnesia <31753563+RetrogradeAmnesia@users.noreply.github.com>
2021-03-13 10:34:18 +1100

    Model Testing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
-
+app.use('/models/', express.static(path.join(__dirname, 'models/')));
 
 const PORT = process.env.PORT || 3000;
 
--- public/client.js
+++ public/client.js
@@ -1,47 +1,102 @@
 import * as THREE from '/build/three.module.js';
 import {OrbitControls} from '/jsm/controls/OrbitControls.js';
 import Stats from '/jsm/libs/stats.module.js';
+import { FBXLoader } from '/jsm/loaders/FBXLoader.js';
 
-const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.z = 2;
+var loadModel = function (model) {
+    let camera, scene, renderer, stats;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    render();
-}, false);
-
-const stats = Stats();
-document.body.appendChild(stats.dom);
-
-var animate = function () {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    const clock = new THREE.Clock();
+    
+    let mixer;
+    
+    const container = document.createElement( 'div' );
+    document.body.appendChild( container );
+    
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.set( 100, 200, 300 );
+    
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xa0a0a0 );
+    scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+    
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+    hemiLight.position.set( 0, 200, 0 );
+    scene.add( hemiLight );
+    
+    const dirLight = new THREE.DirectionalLight( 0xffffff );
+    dirLight.position.set( 0, 200, 100 );
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 180;
+    dirLight.shadow.camera.bottom = - 100;
+    dirLight.shadow.camera.left = - 120;
+    dirLight.shadow.camera.right = 120;
+    scene.add( dirLight );
+    
+    // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+    
+    // ground
+    const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+    mesh.rotation.x = - Math.PI / 2;
+    mesh.receiveShadow = true;
+    scene.add( mesh );
+    
+    const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    scene.add( grid );
+    
+    // model
+    const loader = new FBXLoader();
+    loader.load(model, function ( object ) {
+        mixer = new THREE.AnimationMixer( object );
+        const action = mixer.clipAction( object.animations[ 0 ] );
+        action.play();
+        object.traverse( function ( child ) {
+            if ( child.isMesh ) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        } );
+        scene.add( object );
+    } );
+    
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMap.enabled = true;
+    container.appendChild( renderer.domElement );
+    
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 100, 0 );
     controls.update();
-    render();
-    stats.update();
-};
-
-function render() {
-    renderer.render(scene, camera);
+    
+    window.addEventListener( 'resize', onWindowResize );
+    
+    // stats
+    stats = new Stats();
+    container.appendChild( stats.dom );
+    
+    
+    
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    };
+    
+    //
+    
+    var animate = function () {
+        requestAnimationFrame( animate );
+        const delta = clock.getDelta();
+        if ( mixer ) mixer.update( delta );
+        renderer.render( scene, camera );
+        stats.update();
+    };
+    
+    animate();
 }
 
-animate();
+loadModel('./models/Idle.fbx')
