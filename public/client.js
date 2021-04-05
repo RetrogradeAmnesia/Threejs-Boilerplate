3aff6db798168e8ba3f541f38ea155b1b10ea867
Retrograde Amnesia <31753563+RetrogradeAmnesia@users.noreply.github.com>
2021-04-05 19:07:39 +1000

    Update

--- public/client.js
+++ public/client.js
@@ -2,29 +2,33 @@ import * as THREE from '/build/three.module.js';
 import {OrbitControls} from '/jsm/controls/OrbitControls.js';
 import Stats from '/jsm/libs/stats.module.js';
 import { FBXLoader } from '/jsm/loaders/FBXLoader.js';
+import { unzipSync } from '/jsm/libs/fflate.module.min.js';
+import { WEBGL } from '/jsm/WebGL.js';
 
 
-var loadModel = function (model) {
+
+function logMessage(messages){
+    messages.foreach(message => {
+        console.log(message)
+        return message
+    })
+    console.log(messages)
+};
+
+var loadFBX = function (model) {
     let camera, scene, renderer, stats;
-
-    const clock = new THREE.Clock();
-    
-    let mixer;
-    
+    const clock = new THREE.Clock();    
+    let mixer;    
     const container = document.createElement( 'div' );
-    document.body.appendChild( container );
-    
+    document.body.appendChild( container );    
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-    camera.position.set( 100, 200, 300 );
-    
+    camera.position.set( 100, 200, 300 );    
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xa0a0a0 );
-    scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
-    
+    scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );    
     const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
     hemiLight.position.set( 0, 200, 0 );
-    scene.add( hemiLight );
-    
+    scene.add( hemiLight );    
     const dirLight = new THREE.DirectionalLight( 0xffffff );
     dirLight.position.set( 0, 200, 100 );
     dirLight.castShadow = true;
@@ -32,16 +36,14 @@ var loadModel = function (model) {
     dirLight.shadow.camera.bottom = - 100;
     dirLight.shadow.camera.left = - 120;
     dirLight.shadow.camera.right = 120;
-    scene.add( dirLight );
-    
+    scene.add( dirLight );    
     // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
     
     // ground
     const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
     mesh.rotation.x = - Math.PI / 2;
     mesh.receiveShadow = true;
-    scene.add( mesh );
-    
+    scene.add( mesh );    
     const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
     grid.material.opacity = 0.2;
     grid.material.transparent = true;
@@ -77,9 +79,7 @@ var loadModel = function (model) {
     // stats
     stats = new Stats();
     container.appendChild( stats.dom );
-    
-    
-    
+        
     function onWindowResize() {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
@@ -99,4 +99,99 @@ var loadModel = function (model) {
     animate();
 }
 
-loadModel('./models/Idle.fbx')
+
+// loadFBX('./models/Idle.fbx');
+
+
+
+var loadMRI = function (mriZIP) {
+
+    if ( WEBGL.isWebGL2Available() === false ) {
+        document.body.appendChild( WEBGL.getWebGL2ErrorMessage() );
+    }
+    let camera, scene, mesh, renderer, stats;
+    const planeWidth = 50;
+    const planeHeight = 50;
+    let depthStep = 0.4;
+
+    const container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 2000 );
+    camera.position.z = 70;
+
+    scene = new THREE.Scene();
+
+    // width 256, height 256, depth 109, 8-bit, zip archived raw data
+
+    new THREE.FileLoader()
+        .setResponseType( 'arraybuffer' )
+        .load(mriZIP, function ( data ) {
+
+            const zip = unzipSync( new Uint8Array( data ) );
+            const array = new Uint8Array( zip[ 'head256x256x109' ].buffer );
+
+            const texture = new THREE.DataTexture2DArray( array, 256, 256, 109 );
+            texture.format = THREE.RedFormat;
+            texture.type = THREE.UnsignedByteType;
+
+            const material = new THREE.ShaderMaterial( {
+                uniforms: {
+                    diffuse: { value: texture },
+                    depth: { value: 55 },
+                    size: { value: new THREE.Vector2( planeWidth, planeHeight ) }
+                },
+                // vertexShader: document.getElementById( 'vs' ).textContent.trim(),
+                // fragmentShader: document.getElementById( 'fs' ).textContent.trim(),
+                glslVersion: THREE.GLSL3
+            } );
+
+            const geometry = new THREE.PlaneGeometry( planeWidth, planeHeight );
+
+            mesh = new THREE.Mesh( geometry, material );
+
+            scene.add( mesh );
+
+        } );
+
+    // 2D Texture array is available on WebGL 2.0
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    stats = new Stats();
+    container.appendChild( stats.dom );
+
+    window.addEventListener( 'resize', onWindowResize );
+
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    var animate = function () {
+        requestAnimationFrame( animate );
+        if ( mesh ) {
+            let value = mesh.material.uniforms[ "depth" ].value;
+            value += depthStep;
+            if ( value > 109.0 || value < 0.0 ) {
+                if ( value > 1.0 ) value = 109.0 * 2.0 - value;
+                if ( value < 0.0 ) value = - value;
+                depthStep = - depthStep;
+            }
+            mesh.material.uniforms[ "depth" ].value = value;
+        }
+        renderer.render( scene, camera );
+        stats.update();
+    }
+
+    animate();
+};
+
+
+loadMRI('/models/head256x256x109.zip')
+
